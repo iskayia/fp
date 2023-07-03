f110e54d82e55991ee99a804238586169b5bde51
Iskayia <siska.ti441@gmail.com>
2023-07-03 13:23:14 +0700

    d26

--- resources/views/ecom/tampilan_jual.blade.php
+++ resources/views/ecom/tampilan_jual.blade.php
@@ -1,44 +1,59 @@
 @include('template/header')
+
+<script>
+    function redirectToPage(url) {
+        window.location.href = url;
+    }
+</script>
 <!-- Masthead-->
 <header class="">
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+                aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
+                aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
+                aria-label="Slide 7"></button>
         </div>
         <div class="carousel-inner " role="listbox">
             <div class="carousel-item active">
-                <img src="{{asset('tempenjualan/assets/img/jumbo1.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo1.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/jumbo2.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo2.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/jumbo3.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo3.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/jumbo4.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo4.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/jumbo5.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo5.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/jumbo6.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/jumbo6.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{asset('tempenjualan/assets/img/parfume.jpg')}}" class="d-block w-100" alt="...">
+                <img src="{{ asset('tempenjualan/assets/img/parfume.jpg') }}" class="d-block w-100" alt="...">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -46,11 +61,12 @@
     <!-- <div class="container">
             <div class="">Selamat Datang di Fitri Parfum!</div>
             <div class="masthead-heading text-uppercase">Warnai Harimu dengan Aroma yang menyegarkan</div>
-            @if(Auth::user() == null)
-            <a class="btn btn-primary btn-xl text-uppercase" href="{{route('login')}}">Login untuk Telusuri Lebih Lanjut</a>
-            @endif
+            @if (Auth::user() == null)
+<a class="btn btn-primary btn-xl text-uppercase" href="{{ route('login') }}">Login untuk Telusuri Lebih Lanjut</a>
+@endif
         </div> -->
 </header>
+
 <!-- Services-->
 <section class="page-section" id="services">
     <div class="container">
@@ -60,7 +76,8 @@
         </div>
         <div class="row text-justify">
             <div class="col-md-4">
-                <img class="img-fluid " style="object-fit: cover;height: 300px;" src="{{asset('tempenjualan/assets/img/vector6.png')}}" alt="temukan produk">
+                <img class="img-fluid " style="object-fit: cover;height: 300px;"
+                    src="{{ asset('tempenjualan/assets/img/vector6.png') }}" alt="temukan produk">
                 <!-- <span class="fa-stack fa-4x">
                     <i class="fas fa-circle fa-stack-2x text-primary"></i>
                     <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
@@ -69,53 +86,63 @@
                 <p class="text-muted">Temukan arfum yang akan anda beli </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid " style="object-fit: cover;height: 300px;" src="{{asset('tempenjualan/assets/img/vector2.png')}}" alt="temukan produk">
+                <img class="img-fluid " style="object-fit: cover;height: 300px;"
+                    src="{{ asset('tempenjualan/assets/img/vector2.png') }}" alt="temukan produk">
                 <h4 class="my-3">Beli Parfum</h4>
                 <p class="text-muted">Masukkan ke keranjang belanja atau beli parfum yang kamu akan pesan</p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid " style="object-fit: cover;height: 300px;" src="{{asset('tempenjualan/assets/img/vector1.png')}}" alt="temukan produk">
+                <img class="img-fluid " style="object-fit: cover;height: 300px;"
+                    src="{{ asset('tempenjualan/assets/img/vector1.png') }}" alt="temukan produk">
                 <h4 class="my-3">Bayar pesanan</h4>
                 <p class="text-muted">Lakukan pembayaran untuk pembelian</p>
             </div>
         </div>
     </div>
 </section>
-<!-- Portfolio Grid-->
+<!-- disini list tampilan produk-->
 <section class="page-section bg-light" id="produk">
     <div class="container">
-        <div class="text-center">
-            <h2 class="section-heading text-uppercase">Produk Toko kami</h2>
-            <h3 class="section-subheading text-muted"></h3>
-        </div>
-    </div>
-    <div class="row row-cols-md-4 row-cols-sm-4 g-2">
-        @forelse ($produk as $p)
-        <div class="col-sm-6">
-            <div class="card">
-                <img class="img-fluid img-thumbnail" style="object-fit: cover;height: 300px;" src="gambar/{{$p->gambar_produk}}" alt="Produk Fitri Parfume">
-                <div class="card-body">
-                    <h5 class="card-title">{{$p->nama_produk}}</h5>
-                    <p class="card-text">Rp.{{number_format($p->harga_produk)}}</p>
-                    @if(Auth::guard('pelanggan')->user() != null)
-                    <form action="{{route('beli_langsung')}}" method="POST">
-                        @csrf
-                        @method('POST')
-                        <input type="hidden" name="id_produk" value="{{$p->id_produk}}">
-                        <input type="hidden" name="jumlah" value="1">
-                        <button class="btn btn-primary" type="submit"><ion-icon name="bag-handle-outline"></ion-icon></button>
-                    </form>
-                    <a href="{{route('add_keranjang', $p->id_produk)}}" class="btn btn-primary"><ion-icon name="cart-outline"></ion-icon></a>
-                    @endif
-                </div>
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase ">Produk Toko kami</h2>
+                <h3 class="section-subheading text-muted border-bottom"></h3>
             </div>
+            <div class="drop-down"></div>
+            <button class="btn btn-primary"><i class="bi bi-list-ul"></i></button>
+            <br>
+            <div class="navbar navbar-light bg-light">
+                <form class="form-inline">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Cari">
+                  <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="bi bi-search"></i></button>
+                </form>
+            </div>
+            <br>
+        </div>
+        <div class="row row-cols-md-4 row-cols-sm-4 g-2">
+            @forelse ($produk as $p)
+                <div class="col-sm-6" onclick="redirectToPage('{{ route('detail_produk', $p->id_produk) }}')">
+                    <div class="card  image-container">
+                        <img class="img-fluid img-thumbnail" style="object-fit: cover;height: 300px;widht:300px;"
+                            src="gambar/{{ $p->gambar_produk }}" alt="Produk Fitri Parfume">
+                        <a href="{{ route('add_keranjang', $p->id_produk) }}"
+                            class="btn btn-primary image-button">Tambah
+                            ke Kerajang<ion-icon name="cart-outline"></ion-icon></a>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $p->nama_produk }}</h5>
+                            <h6 class="card-text" style="color: orange;"> Rp.{{ number_format($p->harga_produk) }}
+                            </h6>
+                            <p class="portfolio-caption-subheading text-muted"> Stok : {{ $p->stok }}</p>
+                        </div>
+                    </div>
 
+                </div>
+            @empty
+                <div class="alert alert-danger">
+                    Data Post belum Tersedia.
+                </div>
+            @endforelse
         </div>
-        @empty
-        <div class="alert alert-danger">
-            Data Post belum Tersedia.
-        </div>
-        @endforelse
     </div>
 </section>
 <!-- About-->
@@ -127,13 +154,16 @@
         <div class="row text-justify ">
             <div class="col-md-2">
                 <h5 class=" text-muted text-justify">Lokasi Toko Fitri Parfum</h5>
-                <p class="text-muted">Jalan Blokeng RT 04 RW 03 Serdang Wetan Legok, Kabupaten Tangerang, Serdang Wetan, Kec. Legok, Kabupaten Tangerang, Banten 15820</p>
+                <p class="text-muted">Jalan Blokeng RT 04 RW 03 Serdang Wetan Legok, Kabupaten Tangerang, Serdang
+                    Wetan, Kec. Legok, Kabupaten Tangerang, Banten 15820</p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid " style="object-fit: cover;height: 300px;" src="{{asset('tempenjualan/assets/img/vector4.png')}}" alt="lokasi kami">
+                <img class="img-fluid " style="object-fit: cover;height: 300px;"
+                    src="{{ asset('tempenjualan/assets/img/vector4.png') }}" alt="lokasi kami">
             </div>
             <div class="col-md-4">
-                <img class="img-fluid " style="object-fit: cover;height: 300px;" src="{{asset('tempenjualan/assets/img/vector3.png')}}" alt="lokasi kami">
+                <img class="img-fluid " style="object-fit: cover;height: 300px;"
+                    src="{{ asset('tempenjualan/assets/img/vector3.png') }}" alt="lokasi kami">
             </div>
             <div class="col-md-2 text-justify">
                 <h5 class=" text-muted">Jam Operasional</h5>
@@ -167,7 +197,8 @@
                 <p class="mb-0">Mari cari parfum yang kamu sukai dan warnai harimu dengan aroma yang menyegarkan </p>
             </div>
             <div class="modal-footer flex-nowrap p-0">
-                <button type="button " data-bs-dismiss="modal" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 border-end"><strong>Oke!</strong></button>
+                <button type="button " data-bs-dismiss="modal"
+                    class="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 border-end"><strong>Oke!</strong></button>
             </div>
         </div>
     </div>
@@ -178,7 +209,8 @@
 <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="close-modal" data-bs-dismiss="modal"><img src="gambar/{{$p->gambar_produk}}" alt="Close modal" /></div>
+            <div class="close-modal" data-bs-dismiss="modal"><img src="gambar/{{ $p->gambar_produk }}"
+                    alt="Close modal" /></div>
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-8">
@@ -186,8 +218,12 @@
                             <!-- Project details-->
                             <h2 class="text-uppercase">Project Name</h2>
                             <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-fluid d-block mx-auto" src="gambar/{{$p->gambar_produk}}" alt="..." />
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                            <img class="img-fluid d-block mx-auto" src="gambar/{{ $p->gambar_produk }}"
+                                alt="..." />
+                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt
+                                repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae,
+                                nostrum, reiciendis facere nemo!</p>
                             <ul class="list-inline">
                                 <li>
                                     <strong>Client:</strong>
@@ -198,7 +234,8 @@
                                     Illustration
                                 </li>
                             </ul>
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+                                type="button">
                                 <i class="fas fa-xmark me-1"></i>
                                 Close Project
                             </button>
